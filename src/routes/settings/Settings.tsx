76f8c580648010e1bc4fff65c4faa7b6727b1fd1
Larissa Soares <larissa.soares@neugelb.com>
2024-06-07 01:22:41 +0200

    feat: create settings screen

--- src/routes/settings/Settings.tsx
+++ src/routes/settings/Settings.tsx
@@ -1,8 +1,46 @@
 import "./Settings.css";
 import Navigation from "../../components/Navigation/Navigation";
+import Button from "../../components/Button/Button";
+
+const accountLabel = [
+  "Nome",
+  "Sobrenome",
+  "Nome da Organização",
+  "E-mail",
+  "Senha",
+];
+const accountData = [
+  "Ana Catarina",
+  "Nonato Soares",
+  "Centro de Pesquisa Aquática",
+  "anacatarina@meuemail.com.br",
+  "MinhaSenhaAqui",
+];
 
 export const Settings = () => {
-  return <Navigation tab={undefined} />;
+  return (
+    <div className="settings-container">
+      <Navigation tab={undefined} />
+      <div className="settings-content">
+        <h1>Configurações</h1>
+        <h2 className="settings-subheading">Detalhes da Conta</h2>
+        <div className="settings-account-info">
+          <div className="account-label">
+            {accountLabel.map((text, idx) => (
+              <p key={idx}>{text}</p>
+            ))}
+          </div>
+          <div className="account-data">
+            {accountData.map((text, idx) => (
+              <p key={idx}>{text}</p>
+            ))}
+          </div>
+        </div>
+
+        <Button className="settings-logout-btn" text="Sair" variant="outline" />
+      </div>
+    </div>
+  );
 };
 
 export default Settings;
